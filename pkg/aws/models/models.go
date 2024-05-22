977529c3f1a367a014a199427e30d829f71fd0f2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-05-22 15:40:59 +0300

    aws/models: tag aws models with `nullzero` where needed

--- pkg/aws/models/models.go
+++ pkg/aws/models/models.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"database/sql"
-
 	coremodels "github.com/gardener/inventory/pkg/core/models"
 	"github.com/uptrace/bun"
 )
@@ -40,7 +38,7 @@ type VPC struct {
 	VpcID      string `bun:"vpc_id,notnull,unique"`
 	State      string `bun:"state,notnull"`
 	IPv4CIDR   string `bun:"ipv4_cidr,notnull"`
-	IPv6CIDR   string `bun:"ipv4_cidr"`
+	IPv6CIDR   string `bun:"ipv6_cidr,nullzero"`
 	IsDefault  bool   `bun:"is_default,notnull"`
 	OwnerID    string `bun:"owner_id,notnull"`
 	RegionName string `bun:"region_name,notnull"`
@@ -51,15 +49,15 @@ type Subnet struct {
 	bun.BaseModel `bun:"table:aws_subnet"`
 	coremodels.Model
 
-	Name                   string         `bun:"name,notnull"`
-	SubnetID               string         `bun:"subnet_id,notnull"`
-	VpcID                  string         `bun:"vpc_id,notnull"`
-	State                  string         `bun:"state,notnull"`
-	AZ                     string         `bun:"az,notnull"`
-	AzID                   string         `bun:"az_id,notnull"`
-	AvailableIPv4Addresses int            `bun:"available_ipv4_addresses,notnull"`
-	IPv4CIDR               sql.NullString `bun:"ipv4_cidr,notnull"`
-	IPv6CIDR               sql.NullString `bun:"ipv6_cidr"`
+	Name                   string `bun:"name,notnull"`
+	SubnetID               string `bun:"subnet_id,notnull"`
+	VpcID                  string `bun:"vpc_id,notnull"`
+	State                  string `bun:"state,notnull"`
+	AZ                     string `bun:"az,notnull"`
+	AzID                   string `bun:"az_id,notnull"`
+	AvailableIPv4Addresses int    `bun:"available_ipv4_addresses,notnull"`
+	IPv4CIDR               string `bun:"ipv4_cidr,notnull"`
+	IPv6CIDR               string `bun:"ipv6_cidr,nullzero"`
 }
 
 // Instance represents an AWS EC2 instance
