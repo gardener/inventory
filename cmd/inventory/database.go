f02ecf1a3c5c6617d5a0dee4659fe143fe8d8b8e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-05-22 12:23:21 +0300

    cmd/inventory: add migration related commands
    Partially based on the bun starter kit with some additional changes.

--- cmd/inventory/database.go
+++ cmd/inventory/database.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
 
+	"github.com/gardener/inventory/internal/pkg/migrations"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 	"github.com/uptrace/bun/extra/bundebug"
+	"github.com/uptrace/bun/migrate"
 	"github.com/urfave/cli/v2"
 )
 
@@ -23,15 +28,139 @@ func NewDatabaseCommand() *cli.Command {
 				EnvVars:  []string{"DSN"},
 				Required: true,
 			},
+			&cli.StringFlag{
+				Name:    "migration-dir",
+				Usage:   "path to the directory with migration files",
+				EnvVars: []string{"MIGRATION_DIR"},
+			},
 		},
-		Subcommands: []*cli.Command{},
+		Subcommands: []*cli.Command{
+			{
+				Name:  "init",
+				Usage: "initialize migration tables",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					return migrator.Init(ctx.Context)
+				},
+			},
+			{
+				Name:  "migrate",
+				Usage: "apply pending migrations",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					if err := migrator.Lock(ctx.Context); err != nil {
+						return err
+					}
+					defer migrator.Unlock(ctx.Context)
+
+					group, err := migrator.Migrate(ctx.Context)
+					if err != nil {
+						return err
+					}
+
+					if group.IsZero() {
+						fmt.Printf("database is up to date")
+						return nil
+					}
+
+					fmt.Printf("database migrated to %s\n", group)
+					return nil
+				},
+			},
+			{
+				Name:  "rollback",
+				Usage: "rollback last migration group",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					if err := migrator.Lock(ctx.Context); err != nil {
+						return err
+					}
+					defer migrator.Unlock(ctx.Context)
+
+					group, err := migrator.Rollback(ctx.Context)
+					if err != nil {
+						return err
+					}
+
+					if group.IsZero() {
+						fmt.Printf("there are no migration groups for rollback")
+						return nil
+					}
+
+					fmt.Printf("rolled back %s\n", group)
+					return nil
+				},
+			},
+			{
+				Name:  "lock",
+				Usage: "lock migrations",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					return migrator.Lock(ctx.Context)
+				},
+			},
+			{
+				Name:  "unlock",
+				Usage: "unlock migrations",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					return migrator.Unlock(ctx.Context)
+				},
+			},
+			{
+				Name:  "create",
+				Usage: "create a new migration",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					name := strings.Join(ctx.Args().Slice(), "_")
+					if name == "" {
+						return errors.New("must specify migration description")
+					}
+
+					files, err := migrator.CreateTxSQLMigrations(ctx.Context, name)
+					if err != nil {
+						return err
+					}
+
+					for _, item := range files {
+						fmt.Println(item.Path)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "status",
+				Usage: "display migration status",
+				Action: func(ctx *cli.Context) error {
+					db := newDBFromFlags(ctx)
+					migrator := newMigratorFromFlags(ctx, db)
+					ms, err := migrator.MigrationsWithStatus(ctx.Context)
+					if err != nil {
+						return err
+					}
+
+					fmt.Printf("migrations: %s\n", ms)
+					fmt.Printf("pending migrations: %s\n", ms.Unapplied())
+					fmt.Printf("last migration group: %s\n", ms.LastGroup())
+
+					return nil
+				},
+			},
+		},
 	}
 
 	return cmd
 }
 
-// dbFromFlags returns a Bun database from the specified flags
-func dbFromFlags(ctx *cli.Context) *bun.DB {
+// newDbFromFlags returns a Bun database from the specified flags
+func newDBFromFlags(ctx *cli.Context) *bun.DB {
 	dsn := ctx.String("dsn")
 	debug := ctx.Bool("debug")
 
