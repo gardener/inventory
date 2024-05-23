35b460f807773da0a22c8cfea44097aab78f1045
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-05-23 21:45:47 +0300

    core/registry: add registry for scheduled tasks

--- pkg/core/registry/tasks.go
+++ pkg/core/registry/tasks.go
@@ -4,3 +4,6 @@ import "github.com/hibiken/asynq"
 
 // TaskRegistry is the default registry for tasks.
 var TaskRegistry = New[string, asynq.Handler]()
+
+// ScheduledTaskRegistry is the default registry for scheduled tasks.
+var ScheduledTaskRegistry = New[string, *asynq.Task]()
