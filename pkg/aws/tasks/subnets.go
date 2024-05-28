ec1f834a61f6332a0949c6e6e5ca1386224e9ef0
Niki Dokovski <nickytd@gmail.com>
2024-05-28 12:06:04 +0200

    aws/tasks: add aws:collect-subnets tasks

---
+++ pkg/aws/tasks/subnets.go
@@ -0,0 +1,121 @@
+package tasks
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gardener/inventory/pkg/aws/utils"
+	"github.com/gardener/inventory/pkg/utils/strings"
+	"log/slog"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/gardener/inventory/pkg/aws/clients"
+	"github.com/gardener/inventory/pkg/aws/models"
+	"github.com/hibiken/asynq"
+)
+
+const (
+	AWS_COLLECT_SUBNETS_TYPE        = "aws:collect-subnets"
+	AWS_COLLECT_SUBNETS_REGION_TYPE = "aws:collect-subnets-region"
+)
+
+type CollectSubnetsPayload struct {
+	Region string `json:"region"`
+}
+
+func NewCollectSubnetsTask() *asynq.Task {
+	return asynq.NewTask(AWS_COLLECT_SUBNETS_TYPE, nil)
+}
+
+func NewCollectSubnetsRegionTask(region string) *asynq.Task {
+	if region == "" {
+		return nil
+	}
+	payload, err := json.Marshal(CollectSubnetsPayload{Region: region})
+	if err != nil {
+		return nil
+	}
+	return asynq.NewTask(AWS_COLLECT_SUBNETS_REGION_TYPE, payload)
+}
+
+func HandleCollectSubnetsRegionTask(ctx context.Context, t *asynq.Task) error {
+	var p CollectSubnetsPayload
+	if err := json.Unmarshal(t.Payload(), &p); err != nil {
+		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
+	}
+	return collectSubnetsRegion(ctx, p.Region)
+}
+
+func collectSubnetsRegion(ctx context.Context, region string) error {
+	slog.Info("Collecting AWS subnets", "region", region)
+
+	subnetsOutput, err := clients.Ec2.DescribeSubnets(ctx,
+		&ec2.DescribeSubnetsInput{},
+		func(o *ec2.Options) {
+			o.Region = region
+		},
+	)
+
+	if err != nil {
+		slog.Error("could not describe subnets", "err", err)
+		return err
+	}
+
+	subnets := make([]models.Subnet, 0, len(subnetsOutput.Subnets))
+	for _, s := range subnetsOutput.Subnets {
+		name := utils.FetchTag(s.Tags, "Name")
+		slog.Info("Subnet", "name", name)
+		modelSubnet := models.Subnet{
+			Name:                   name,
+			SubnetID:               strings.StringFromPointer(s.SubnetId),
+			VpcID:                  strings.StringFromPointer(s.VpcId),
+			State:                  string(s.State),
+			AZ:                     strings.StringFromPointer(s.AvailabilityZone),
+			AzID:                   strings.StringFromPointer(s.AvailabilityZoneId),
+			AvailableIPv4Addresses: int(*s.AvailableIpAddressCount),
+			IPv4CIDR:               strings.StringFromPointer(s.CidrBlock), //TODO: this can be nil ? and cause panic
+			IPv6CIDR:               "",                                     //TODO: fetch IPv6 CIDR
+		}
+		subnets = append(subnets, modelSubnet)
+
+	}
+
+	if len(subnets) == 0 {
+		return nil
+	}
+	_, err = clients.Db.NewInsert().
+		Model(&subnets).
+		On("CONFLICT (subnet_id) DO UPDATE").
+		Exec(ctx)
+	if err != nil {
+		slog.Error("could not insert Subnets into db", "err", err)
+		return err
+	}
+
+	return nil
+}
+
+func HandleCollectSubnetsTask(ctx context.Context, t *asynq.Task) error {
+	return collectSubnets(ctx)
+}
+
+func collectSubnets(ctx context.Context) error {
+	// Collect regions from Db
+	regions := make([]models.Region, 0)
+	err := clients.Db.NewSelect().Model(&regions).Scan(ctx)
+	if err != nil {
+		slog.Error("could not select regions from db", "err", err)
+		return err
+	}
+	for _, r := range regions {
+		// Trigger Asynq task for each region
+		subnetTask := NewCollectSubnetsRegionTask(r.Name)
+		info, err := clients.Client.Enqueue(subnetTask)
+		if err != nil {
+			slog.Error("could not enqueue task", "type", subnetTask.Type(), "err", err)
+			continue
+		}
+		slog.Info("enqueued task", "type", subnetTask.Type(), "id", info.ID, "queue", info.Queue)
+	}
+	return nil
+}
