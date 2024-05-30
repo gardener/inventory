6f522730149e1109ef18928cf18e73bb2febf092
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-05-30 15:58:49 +0300

    common/tasks: add task for cleaning up stale records

---
+++ pkg/common/tasks/tasks.go
@@ -0,0 +1,77 @@
+package tasks
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/gardener/inventory/pkg/clients"
+	"github.com/gardener/inventory/pkg/core/config"
+	"github.com/gardener/inventory/pkg/core/registry"
+	"github.com/hibiken/asynq"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	// DeleteStaleRecordsTaskType is the name of the task responsible for
+	// cleaning up stale records from the database.
+	DeleteStaleRecordsTaskType = "common:task:delete-stale-records"
+)
+
+// NewDeleteStaleRecordsTask creates a new task, which deletes stale records.
+func NewDeleteStaleRecordsTask(items []config.ModelRetentionConfig) (*asynq.Task, error) {
+	payload, err := yaml.Marshal(items)
+	if err != nil {
+		return nil, err
+	}
+
+	task := asynq.NewTask(DeleteStaleRecordsTaskType, payload)
+
+	return task, nil
+}
+
+// HandleDeleteStaleRecordsTask deletes records, which have been identified as
+// stale.
+func HandleDeleteStaleRecordsTask(ctx context.Context, task *asynq.Task) error {
+	var items []*config.ModelRetentionConfig
+
+	if err := yaml.Unmarshal(task.Payload(), &items); err != nil {
+		return fmt.Errorf("failed to unmarshal payload: %w", err)
+	}
+
+	if items == nil {
+		return nil
+	}
+
+	for _, item := range items {
+		// Look up the registry for the actual model type
+		model, ok := registry.ModelRegistry.Get(item.Name)
+		if !ok {
+			slog.Warn("model not found in registry", "name", item.Name)
+			continue
+		}
+
+		now := time.Now()
+		past := now.Add(-item.Duration)
+		_, err := clients.Db.NewDelete().
+			Model(model).
+			Where("updated_at < ?", past).
+			Exec(ctx)
+
+		if err != nil {
+			// Simply log the error here and keep going with the
+			// rest of the objects to cleanup
+			slog.Error("failed to delete stale model records", "name", item.Name, "reason", err)
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	registry.TaskRegistry.MustRegister(
+		DeleteStaleRecordsTaskType,
+		asynq.HandlerFunc(HandleDeleteStaleRecordsTask),
+	)
+}
