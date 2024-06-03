5076d5278578f707afb28f5e9be2b606c2955d9d
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-06-03 11:51:22 +0300

    cmd: log duration of task execution
    The task middleware now also logs the duration of task execution.

--- cmd/inventory/utils.go
+++ cmd/inventory/utils.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/hibiken/asynq"
 	"github.com/olekukonko/tablewriter"
@@ -132,8 +133,24 @@ func newLoggingMiddleware() asynq.MiddlewareFunc {
 		mw := func(ctx context.Context, task *asynq.Task) error {
 			taskID, _ := asynq.GetTaskID(ctx)
 			queueName, _ := asynq.GetQueueName(ctx)
-			slog.Info("received task", "id", taskID, "queue", queueName, "name", task.Type())
-			return handler.ProcessTask(ctx, task)
+			taskName := task.Type()
+			slog.Info(
+				"received task",
+				"id", taskID,
+				"queue", queueName,
+				"name", taskName,
+			)
+			start := time.Now()
+			err := handler.ProcessTask(ctx, task)
+			elapsed := time.Since(start)
+			slog.Info(
+				"task finished",
+				"id", taskID,
+				"queue", queueName,
+				"name", taskName,
+				"duration", elapsed,
+			)
+			return err
 		}
 
 		return asynq.HandlerFunc(mw)
