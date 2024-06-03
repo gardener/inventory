3635ca8fe2ae684d6c23972f29e388b33170b70f
Niki Dokovski <nickytd@gmail.com>
2024-06-03 14:38:08 +0200

    tasks: add gardener tasks to task registry

---
+++ pkg/gardener/tasks/tasks.go
@@ -0,0 +1,14 @@
+package tasks
+
+import (
+	"github.com/hibiken/asynq"
+
+	"github.com/gardener/inventory/pkg/core/registry"
+)
+
+func init() {
+	// Task handlers
+	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_PROJECTS_TYPE, asynq.HandlerFunc(HandleGardenerCollectProjectsTask))
+	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_SEEDS_TYPE, asynq.HandlerFunc(HandleGardenerCollectSeedsTask))
+	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_SHOOTS_TYPE, asynq.HandlerFunc(HandleGardenerCollectShootsTask))
+}
