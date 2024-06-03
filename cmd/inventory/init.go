6079fa4ca523d456b159a8c862c290dd288a18a2
Niki Dokovski <nickytd@gmail.com>
2024-06-03 14:38:08 +0200

    tasks: add gardener tasks to task registry

--- cmd/inventory/init.go
+++ cmd/inventory/init.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	// Imports only for their side-effects.
 	_ "github.com/gardener/inventory/pkg/aws/models"
 	_ "github.com/gardener/inventory/pkg/aws/tasks"
 	_ "github.com/gardener/inventory/pkg/common/tasks"
 	_ "github.com/gardener/inventory/pkg/gardener/models"
+	_ "github.com/gardener/inventory/pkg/gardener/tasks"
 )
