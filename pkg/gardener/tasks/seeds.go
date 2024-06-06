96c853009b5d8550a317db7e1e1749e8385f0943
Niki Dokovski <nickytd@gmail.com>
2024-06-06 14:54:00 +0200

    task: add pager to g:task:collect-seeds

--- pkg/gardener/tasks/seeds.go
+++ pkg/gardener/tasks/seeds.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"log/slog"
 
+	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/hibiken/asynq"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/pager"
 
 	"github.com/gardener/inventory/pkg/clients"
 	"github.com/gardener/inventory/pkg/gardener/models"
@@ -37,18 +40,29 @@ func collectSeeds(ctx context.Context) error {
 	if gardenClient == nil {
 		return fmt.Errorf("could not get garden client: %w", asynq.SkipRetry)
 	}
-	seedList, err := gardenClient.CoreV1beta1().Seeds().List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
-	seeds := make([]models.Seed, 0, len(seedList.Items))
-	for _, s := range seedList.Items {
+
+	seeds := make([]models.Seed, 0, 100)
+	err := pager.New(
+		pager.SimplePageFunc(func(opts metav1.ListOptions) (runtime.Object, error) {
+			return gardenClient.CoreV1beta1().Seeds().List(ctx, metav1.ListOptions{})
+		}),
+	).EachListItem(ctx, metav1.ListOptions{}, func(obj runtime.Object) error {
+		s, ok := obj.(*v1beta1.Seed)
+		if !ok {
+			return fmt.Errorf("unexpected object type: %T", obj)
+		}
 		seed := models.Seed{
 			Name:              s.Name,
 			KubernetesVersion: strings.StringFromPointer(s.Status.KubernetesVersion),
 		}
 		seeds = append(seeds, seed)
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("could not list seeds: %w", err)
 	}
+
 	if len(seeds) == 0 {
 		return nil
 	}
