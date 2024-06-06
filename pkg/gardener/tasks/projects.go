63b93c71f5cbb650c003834720a762b46e596335
Niki Dokovski <nickytd@gmail.com>
2024-06-06 14:53:37 +0200

    task: add pager to g:task:collect-projects

--- pkg/gardener/tasks/projects.go
+++ pkg/gardener/tasks/projects.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log/slog"
 
+	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/hibiken/asynq"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/pager"
 
 	"github.com/gardener/inventory/pkg/clients"
 	"github.com/gardener/inventory/pkg/gardener/models"
@@ -34,21 +37,32 @@ func collectProjects(ctx context.Context) error {
 	if gardenClient == nil {
 		return fmt.Errorf("could not get garden client: %w", asynq.SkipRetry)
 	}
-	projectList, err := gardenClient.CoreV1beta1().Projects().List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
-	projects := make([]models.Project, 0, len(projectList.Items))
-	for _, p := range projectList.Items {
-		projectModel := models.Project{
+
+	projects := make([]models.Project, 0, 100)
+	err := pager.New(
+		pager.SimplePageFunc(func(opts metav1.ListOptions) (runtime.Object, error) {
+			return gardenClient.CoreV1beta1().Projects().List(ctx, opts)
+		}),
+	).EachListItem(ctx, metav1.ListOptions{}, func(obj runtime.Object) error {
+		p, ok := obj.(*v1beta1.Project)
+		if !ok {
+			return fmt.Errorf("unexpected object type: %T", obj)
+		}
+		project := models.Project{
 			Name:      p.Name,
 			Namespace: p.Namespace,
 			Status:    string(p.Status.Phase),
 			Purpose:   strings.StringFromPointer(p.Spec.Purpose),
 			Owner:     p.Spec.Owner.Name,
 		}
-		projects = append(projects, projectModel)
+		projects = append(projects, project)
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("could not list projects: %w", err)
 	}
+
 	if len(projects) == 0 {
 		return nil
 	}
