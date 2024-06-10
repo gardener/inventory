081a4069dba29be51c23f7b07d752a3a362cc28e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-06-10 10:37:35 +0300

    g/tasks: add meta task for collecting all Gardener objects

--- pkg/gardener/tasks/tasks.go
+++ pkg/gardener/tasks/tasks.go
@@ -1,10 +1,31 @@
 package tasks
 
 import (
+	"context"
+
 	"github.com/hibiken/asynq"
 
+	"github.com/gardener/inventory/pkg/common/utils"
 	"github.com/gardener/inventory/pkg/core/registry"
 )
+
+const (
+	// GardenerCollectAllTaskType is a meta task, which enqueues all
+	// relevant Gardener tasks.
+	GardenerCollectAllTaskType = "g:task:collect-all"
+)
+
+func HandleCollectAllTask(ctx context.Context, t *asynq.Task) error {
+	// Task constructors
+	taskFns := []utils.TaskConstructor{
+		NewGardenerCollectProjectsTask,
+		NewGardenerCollectSeedsTask,
+		NewGardenerCollectShootsTask,
+		NewGardenerCollectMachinesTask,
+	}
+
+	return utils.Enqueue(taskFns)
+}
 
 func init() {
 	// Task handlers
@@ -13,4 +34,5 @@ func init() {
 	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_SHOOTS_TYPE, asynq.HandlerFunc(HandleGardenerCollectShootsTask))
 	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_MACHINES_TYPE, asynq.HandlerFunc(HandleGardenerCollectMachinesTask))
 	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_MACHINES_SEED_TYPE, asynq.HandlerFunc(HandleGardenerCollectMachinesForSeedTask))
+	registry.TaskRegistry.MustRegister(GardenerCollectAllTaskType, asynq.HandlerFunc(HandleCollectAllTask))
 }
