c4a43cd7eac151254d864122c94a76f27516d8a7
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-06-12 11:33:30 +0300

    migrations: add view for orphan AWS EC2 instances

---
+++ internal/pkg/migrations/20240612081637_add_view_for_orphaned_aws_instances.tx.up.sql
@@ -0,0 +1,13 @@
+CREATE VIEW aws_orphan_instance AS
+SELECT
+        i.*,
+        v.name AS vpc_name,
+        v.region_name AS region,
+
+        s.name AS shoot_name,
+        s.project_name AS project_name
+FROM aws_instance AS i
+LEFT JOIN g_machine AS m ON i.name = m.name
+LEFT JOIN aws_vpc AS v ON i.vpc_id = v.vpc_id
+LEFT JOIN g_shoot AS s ON v.name = s.technical_id
+WHERE m.name IS NULL;
