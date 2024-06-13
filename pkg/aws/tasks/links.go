c40ab60bc888f9e8dd6c4b969bfdd154bd5cedf8
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-06-13 10:03:01 +0300

    aws: add function for linking Region with VPC

--- pkg/aws/tasks/links.go
+++ pkg/aws/tasks/links.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/gardener/inventory/pkg/aws/models"
 	"github.com/uptrace/bun"
@@ -29,12 +30,58 @@ func LinkAvailabilityZoneWithRegion(ctx context.Context, db *bun.DB) error {
 		links = append(links, link)
 	}
 
-	_, err = db.NewInsert().
+	out, err := db.NewInsert().
 		Model(&links).
 		On("CONFLICT (region_id, az_id) DO UPDATE").
 		Set("updated_at = EXCLUDED.updated_at").
 		Returning("id").
 		Exec(ctx)
+
+	count, err := out.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	slog.Info("linked aws region with az", "count", count)
+
+	return err
+}
+
+// LinkRegionWithVPC creates links between the AWS Region and VPC
+func LinkRegionWithVPC(ctx context.Context, db *bun.DB) error {
+	var vpcs []models.VPC
+	err := db.NewSelect().
+		Model(&vpcs).
+		Relation("Region").
+		Where("region.id IS NOT NULL").
+		Scan(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	links := make([]models.RegionToVPC, 0, len(vpcs))
+	for _, vpc := range vpcs {
+		link := models.RegionToVPC{
+			VpcID:    vpc.ID,
+			RegionID: vpc.Region.ID,
+		}
+		links = append(links, link)
+	}
+
+	out, err := db.NewInsert().
+		Model(&links).
+		On("CONFLICT (region_id, vpc_id) DO UPDATE").
+		Set("updated_at = EXCLUDED.updated_at").
+		Returning("id").
+		Exec(ctx)
+
+	count, err := out.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	slog.Info("linked aws region with vpc", "count", count)
 
 	return err
 }
