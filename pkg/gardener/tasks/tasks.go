217f2509f3942b5797b985a5b0ba1b2c77fdc4a2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-06-14 09:07:17 +0300

    g/tasks: add new task for linking all Gardener related objects
    New task has been added: g:task:link-all

--- pkg/gardener/tasks/tasks.go
+++ pkg/gardener/tasks/tasks.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hibiken/asynq"
 
+	"github.com/gardener/inventory/pkg/clients"
 	"github.com/gardener/inventory/pkg/common/utils"
 	"github.com/gardener/inventory/pkg/core/registry"
 )
@@ -13,8 +14,14 @@ const (
 	// GardenerCollectAllTaskType is a meta task, which enqueues all
 	// relevant Gardener tasks.
 	GardenerCollectAllTaskType = "g:task:collect-all"
+
+	// GardenerLinkAllTaskType is the task type for linking all Gardener
+	// related objects.
+	GardenerLinkAllTaskType = "g:task:link-all"
 )
 
+// HandleCollectAllTask is the handler, which enqueues tasks for collecting all
+// known Gardener resources.
 func HandleCollectAllTask(ctx context.Context, t *asynq.Task) error {
 	// Task constructors
 	taskFns := []utils.TaskConstructor{
@@ -27,6 +34,18 @@ func HandleCollectAllTask(ctx context.Context, t *asynq.Task) error {
 	return utils.Enqueue(taskFns)
 }
 
+// HandleLinkAllTask is the handler, which establishes relationships between the
+// various Gardener models.
+func HandleLinkAllTask(ctx context.Context, r *asynq.Task) error {
+	linkFns := []utils.LinkFunction{
+		LinkShootWithProject,
+		LinkShootWithSeed,
+		LinkMachineWithShoot,
+	}
+
+	return utils.LinkObjects(ctx, clients.Db, linkFns)
+}
+
 func init() {
 	// Task handlers
 	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_PROJECTS_TYPE, asynq.HandlerFunc(HandleGardenerCollectProjectsTask))
@@ -35,4 +54,5 @@ func init() {
 	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_MACHINES_TYPE, asynq.HandlerFunc(HandleGardenerCollectMachinesTask))
 	registry.TaskRegistry.MustRegister(GARDENER_COLLECT_MACHINES_SEED_TYPE, asynq.HandlerFunc(HandleGardenerCollectMachinesForSeedTask))
 	registry.TaskRegistry.MustRegister(GardenerCollectAllTaskType, asynq.HandlerFunc(HandleCollectAllTask))
+	registry.TaskRegistry.MustRegister(GardenerLinkAllTaskType, asynq.HandlerFunc(HandleLinkAllTask))
 }
