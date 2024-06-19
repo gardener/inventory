00175e7b9644c1037f30857fa078b8a184233589
Bozhidar Atanasov <bozhidar.atanasov@sap.com>
2024-06-19 15:00:03 +0300

    utils/strings: add unit tests for StringFromPointer

---
+++ pkg/utils/strings/strings_test.go
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package strings
+
+import (
+	"testing"
+)
+
+func TestStringFromPointer(t *testing.T) {
+	emptyString := ""
+	nonEmptyString := "abc"
+	flagtests := []struct {
+		in  *string
+		out string
+	}{
+		{nil, ""},
+		{&emptyString, ""},
+		{&nonEmptyString, nonEmptyString},
+	}
+
+	for _, tt := range flagtests {
+		out := StringFromPointer(tt.in)
+
+		if tt.out != out {
+			t.Fatalf(`StringFromPointer(%v) == %q, expected %q.`, tt.in, out, tt.out)
+		}
+	}
+}
