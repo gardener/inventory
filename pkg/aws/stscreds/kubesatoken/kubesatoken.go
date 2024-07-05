e9dbea7506b7884ec646e9ab93826e7653ba4c45
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-07-05 12:40:57 +0300

    aws/stscreds/kubesatoken: add NewCredentialsProvider
    The `NewCredentialsProvider` constructor is used to create a new Web Identity
    Credentials Provider based on stscreds.WebIdentityRoleProvider using provided
    spec.

--- pkg/aws/stscreds/kubesatoken/kubesatoken.go
+++ pkg/aws/stscreds/kubesatoken/kubesatoken.go
@@ -25,7 +25,9 @@ import (
 	"errors"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	authenticationv1 "k8s.io/api/authentication/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -33,9 +35,22 @@ import (
 	"k8s.io/utils/ptr"
 )
 
-// ErrNoServiceAccount is returned when the [KubeSATokenRetriever] was
-// configured without a service account.
+// ErrNoServiceAccount is an error, which is returned when the
+// [TokenRetriever] was configured without a service account.
 var ErrNoServiceAccount = errors.New("no service account specified")
+
+// ErrNoSTSClient is an error, which is returned when creating a new credentials
+// provider without the required AWS STS client.
+var ErrNoSTSClient = errors.New("no STS client specified")
+
+// ErrNoRoleARN is an error, which is returned when creating a new credentials
+// provider without specifying a IAM Role ARN.
+var ErrNoRoleARN = errors.New("no IAM Role ARN specified")
+
+// ErrNoTokenRetriever is an error, which is returned when creating a new
+// credentials provider, without specifying a [stscreds.IdentityTokenRetriever]
+// implementation.
+var ErrNoTokenRetriever = errors.New("no token retriever specified")
 
 // TokenRetriever retrieves a service account token from Kubernetes, which can
 // later be used to request temporary security credentials to access the AWS
@@ -190,4 +205,57 @@ func WithClient(client *kubernetes.Clientset) TokenRetrieverOption {
 	}
 
 	return opt
+}
+
+// CredentialsProviderSpec provides the configuration settings for the Web
+// Identity Credentials Provider.
+type CredentialsProviderSpec struct {
+	// Client is the API client used to make API calls to the AWS STS.
+	Client *sts.Client
+
+	// RoleARN is the IAM Role ARN to assume.
+	RoleARN string
+
+	// RoleSessionName is the name of the session, which uniquely identifies it
+	RoleSessionName string
+
+	// Duration specifies the expiry duration of the STS credentials.
+	Duration time.Duration
+
+	// TokenRetriever is the identity token retriever implementation to use.
+	TokenRetriever stscreds.IdentityTokenRetriever
+}
+
+// NewCredentialsProvider creates a new [stscreds.CredentialsProvider] based on
+// the provided spec.
+func NewCredentialsProvider(spec *CredentialsProviderSpec) (aws.CredentialsProvider, error) {
+	if spec.Client == nil {
+		return nil, ErrNoSTSClient
+	}
+
+	if spec.RoleARN == "" {
+		return nil, ErrNoRoleARN
+	}
+
+	if spec.TokenRetriever == nil {
+		return nil, ErrNoTokenRetriever
+	}
+
+	opts := []func(o *stscreds.WebIdentityRoleOptions){
+		func(o *stscreds.WebIdentityRoleOptions) {
+			o.Duration = spec.Duration
+		},
+		func(o *stscreds.WebIdentityRoleOptions) {
+			o.RoleSessionName = spec.RoleSessionName
+		},
+	}
+
+	provider := stscreds.NewWebIdentityRoleProvider(
+		spec.Client,
+		spec.RoleARN,
+		spec.TokenRetriever,
+		opts...,
+	)
+
+	return provider, nil
 }
