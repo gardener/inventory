ac8594c8a8591ffab83b209a4cd63d1af0ff26f5
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-07-30 17:53:15 +0300

    migrations: typo fix in migration

--- internal/pkg/migrations/20240730113820_add_link_table_aws_instance_to_network_interface.tx.up.sql
+++ internal/pkg/migrations/20240730113820_add_link_table_aws_instance_to_network_interface.tx.up.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS "l_aws_instance_to_net_interface" (
     "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("instance_id") REFERENCES "aws_instance" ("id") ON DELETE CASCADE,
-    FOREIGN KEY ("ni_id") REFERENCES "aws_net_interface" ("id") ON DELETE CASCADE
+    FOREIGN KEY ("ni_id") REFERENCES "aws_net_interface" ("id") ON DELETE CASCADE,
     CONSTRAINT "l_aws_instance_to_net_interface_key" UNIQUE ("instance_id", "ni_id")
 );
