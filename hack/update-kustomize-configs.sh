cfdf5835a23102fd83d95111bd3dcf365106a1ba
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-08-07 10:21:47 +0300

    hack: add script for syncing sample configs with kustomize variants

---
+++ hack/update-kustomize-configs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# The following script is used to sync the configuration files used by the
+# existing kustomize variants in `deployment/kustomize' with the sample config
+# and data files from the repo, e.g. sample inventory config, dashboards, etc.
+
+set -e
+
+_SCRIPT_NAME="${0##*/}"
+_SCRIPT_DIR=$( dirname `readlink -f -- "${0}"` )
+_REPO_ROOT="${_SCRIPT_DIR}/.."
+_KUSTOMIZE_DIR="${_REPO_ROOT}/deployment/kustomize"
+
+# Update sample config
+install -m 0644 \
+        "${_REPO_ROOT}/examples/config.yaml" \
+        "${_KUSTOMIZE_DIR}/config/secrets/config.yaml"
+
+# Update Grafana dashboards
+rsync -av --exclude "*~" \
+      "${_REPO_ROOT}/extra/grafana/" \
+      "${_KUSTOMIZE_DIR}/grafana/config/files"
+
+# Update Prometheus configs
+install -m 0644 \
+        "${_REPO_ROOT}/extra/prometheus/prometheus.yml" \
+        "${_KUSTOMIZE_DIR}/prometheus/config/files/prometheus.yml"
