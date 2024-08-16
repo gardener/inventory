b7ad8aa01436972e42cc4f9f7a20736458e1b455
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-08-16 11:51:33 +0300

    aws/tasks: refactor `aws:task:collect-images` task
    Remove the `aws:task:collect-images-region` task.
    
    The `aws:task:collect-images` accepts a payload now, which makes the former task
    obsolete.

--- pkg/aws/tasks/images.go
+++ pkg/aws/tasks/images.go
@@ -6,90 +6,144 @@ package tasks
 
 import (
 	"context"
-	"errors"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hibiken/asynq"
-	"gopkg.in/yaml.v3"
 
 	"github.com/gardener/inventory/pkg/aws/constants"
 	"github.com/gardener/inventory/pkg/aws/models"
+	awsutils "github.com/gardener/inventory/pkg/aws/utils"
 	asynqclient "github.com/gardener/inventory/pkg/clients/asynq"
-	awsclient "github.com/gardener/inventory/pkg/clients/aws"
+	awsclients "github.com/gardener/inventory/pkg/clients/aws"
 	"github.com/gardener/inventory/pkg/clients/db"
-	"github.com/gardener/inventory/pkg/utils/strings"
+	asynqutils "github.com/gardener/inventory/pkg/utils/asynq"
+	stringutils "github.com/gardener/inventory/pkg/utils/strings"
 )
 
 const (
-	TaskCollectImages          = "aws:task:collect-images"
-	TaskCollectImagesForRegion = "aws:task:collect-images-region"
+	// TaskCollectImages is the name of the task for collecting AWS AMIs.
+	TaskCollectImages = "aws:task:collect-images"
 )
 
-// ErrMissingOwners is returned when expected owner names are missing.
-var ErrMissingOwners = errors.New("missing owner names")
-
+// CollectImagesPayload is the payload, which is used for collecting AWS AMIs.
 type CollectImagesPayload struct {
-	ImageOwners []string `yaml:"image_owners"`
+	// Region specifies the region from which to collect.
+	Region string `json:"region" yaml:"region"`
+
+	// AccountID specifies the AWS Account ID, which is associated with a
+	// registered client.
+	AccountID string `json:"account_id" yaml:"account_id"`
+
+	// Owners specifies owners of AMI images. Only images with the specified
+	// owners will be collected.
+	Owners []string `json:"owners" yaml:"owners"`
 }
 
-type CollectImagesForRegionPayload struct {
-	Region      string   `yaml:"region"`
-	ImageOwners []string `yaml:"image_owners"`
-}
-
-// NewCollectImagesTask creates a new task for collecting AMI Images from
-// all AWS Regions.
-func NewCollectImagesTask() *asynq.Task {
-	return asynq.NewTask(TaskCollectImages, nil)
-}
-
-// NewCollectImagesForRegionTask creates a new task for collecting AMI
-// Images for a given AWS Region.
-func NewCollectImagesForRegionTask(payload CollectImagesForRegionPayload) (*asynq.Task, error) {
-	if payload.Region == "" {
-		return nil, ErrMissingRegion
+// HandleCollectImagesTask handles the task for collecting AWS AMIs.
+func HandleCollectImagesTask(ctx context.Context, t *asynq.Task) error {
+	// If we were called without a specified region and account id, then we
+	// will enqueue tasks for collecting AWS AMIs from all known regions.
+	data := t.Payload()
+	var payload CollectImagesPayload
+	if err := asynqutils.Unmarshal(data, &payload); err != nil {
+		return asynqutils.SkipRetry(err)
 	}
 
-	if len(payload.ImageOwners) == 0 {
-		return nil, ErrMissingOwners
+	switch {
+	case payload.Region == "":
+		// We don't have a specified region, enqueue collection for all
+		// known regions.
+		return enqueueCollectImages(ctx, payload)
+	case payload.AccountID == "":
+		// Required AccountID is missing
+		return asynqutils.SkipRetry(ErrNoAccountID)
+	default:
+		// Collect AMIs
+		return collectImages(ctx, payload)
 	}
+}
 
-	rawPayload, err := yaml.Marshal(payload)
+// enqueueCollectImages enqueues tasks for collecting AWS AMIs from all known
+// regions and accounts.
+func enqueueCollectImages(ctx context.Context, payload CollectImagesPayload) error {
+	regions, err := awsutils.GetRegionsFromDB(ctx)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("failed to get regions: %w", err)
 	}
 
-	return asynq.NewTask(TaskCollectImagesForRegion, rawPayload), nil
+	// Enqueue task for each known region
+	for _, r := range regions {
+		// By default we will specify the current account id as the
+		// image owner, unless specified as part of the payload.
+		owners := payload.Owners
+		if len(owners) == 0 {
+			owners = []string{r.AccountID}
+		}
+		payload := CollectImagesPayload{
+			Region:    r.Name,
+			AccountID: r.AccountID,
+			Owners:    owners,
+		}
+		data, err := json.Marshal(payload)
+		if err != nil {
+			slog.Error(
+				"failed to marshal payload for AWS AMIs",
+				"region", r.Name,
+				"account_id", r.AccountID,
+				"reason", err,
+			)
+			continue
+		}
+
+		task := asynq.NewTask(TaskCollectImages, data)
+		info, err := asynqclient.Client.Enqueue(task)
+		if err != nil {
+			slog.Error(
+				"failed to enqueue task",
+				"type", task.Type(),
+				"region", r.Name,
+				"account_id", r.AccountID,
+				"reason", err,
+			)
+			continue
+		}
+
+		slog.Info(
+			"enqueued task",
+			"type", task.Type(),
+			"id", info.ID,
+			"queue", info.Queue,
+			"region", r.Name,
+			"account_id", r.AccountID,
+		)
+	}
+
+	return nil
 }
 
-// HandleCollectImagesForRegionTask collects EC2 Images for a specific Region.
-func HandleCollectImagesForRegionTask(ctx context.Context, t *asynq.Task) error {
-	var payload CollectImagesForRegionPayload
-	if err := yaml.Unmarshal(t.Payload(), &payload); err != nil {
-		return fmt.Errorf("yaml.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
+// collectImages collects the AWS AMIs based on the specified payload.
+func collectImages(ctx context.Context, payload CollectImagesPayload) error {
+	if payload.AccountID == "" {
+		return asynqutils.SkipRetry(ErrNoAccountID)
 	}
 
 	if payload.Region == "" {
-		return ErrMissingRegion
+		return asynqutils.SkipRetry(ErrNoRegion)
 	}
 
-	if len(payload.ImageOwners) == 0 {
-		return ErrMissingOwners
+	client, ok := awsclients.EC2Clientset.Get(payload.AccountID)
+	if !ok {
+		return asynqutils.SkipRetry(ClientNotFound(payload.AccountID))
 	}
 
-	return collectImagesForRegion(ctx, payload)
-}
-
-func collectImagesForRegion(ctx context.Context, payload CollectImagesForRegionPayload) error {
-	region := payload.Region
-	slog.Info("Collecting AWS AMI ", "region", region)
 	paginator := ec2.NewDescribeImagesPaginator(
-		awsclient.EC2,
+		client.Client,
 		&ec2.DescribeImagesInput{
-			Owners: payload.ImageOwners,
+			Owners: payload.Owners,
 		},
 		func(params *ec2.DescribeImagesPaginatorOptions) {
 			params.Limit = int32(constants.PageSize)
@@ -103,29 +157,36 @@ func collectImagesForRegion(ctx context.Context, payload CollectImagesForRegionP
 		page, err := paginator.NextPage(
 			ctx,
 			func(o *ec2.Options) {
-				o.Region = region
+				o.Region = payload.Region
 			},
 		)
 
 		if err != nil {
-			slog.Error("could not describe images", "region", region, "reason", err)
+			slog.Error(
+				"could not describe AMIs",
+				"region", payload.Region,
+				"account_id", payload.AccountID,
+				"reason", err,
+			)
 			return err
 		}
+
 		items = append(items, page.Images...)
 	}
 
 	images := make([]models.Image, 0, len(items))
 	for _, image := range items {
-		modelImage := models.Image{
-			ImageID:        strings.StringFromPointer(image.ImageId),
-			Name:           strings.StringFromPointer(image.Name),
-			OwnerID:        strings.StringFromPointer(image.OwnerId),
+		item := models.Image{
+			ImageID:        stringutils.StringFromPointer(image.ImageId),
+			AccountID:      payload.AccountID,
+			Name:           stringutils.StringFromPointer(image.Name),
+			OwnerID:        stringutils.StringFromPointer(image.OwnerId),
 			ImageType:      string(image.ImageType),
 			RootDeviceType: string(image.RootDeviceType),
-			Description:    strings.StringFromPointer(image.Description),
-			RegionName:     region,
+			Description:    stringutils.StringFromPointer(image.Description),
+			RegionName:     payload.Region,
 		}
-		images = append(images, modelImage)
+		images = append(images, item)
 	}
 
 	if len(images) == 0 {
@@ -134,7 +195,7 @@ func collectImagesForRegion(ctx context.Context, payload CollectImagesForRegionP
 
 	out, err := db.DB.NewInsert().
 		Model(&images).
-		On("CONFLICT (image_id) DO UPDATE").
+		On("CONFLICT (image_id, account_id) DO UPDATE").
 		Set("name = EXCLUDED.name").
 		Set("owner_id = EXCLUDED.owner_id").
 		Set("image_type = EXCLUDED.image_type").
@@ -146,7 +207,12 @@ func collectImagesForRegion(ctx context.Context, payload CollectImagesForRegionP
 		Exec(ctx)
 
 	if err != nil {
-		slog.Error("could not insert images into db", "region", region, "reason", err)
+		slog.Error(
+			"could not insert AMIs into db",
+			"region", payload.Region,
+			"account_id", payload.AccountID,
+			"reason", err,
+		)
 		return err
 	}
 
@@ -155,65 +221,12 @@ func collectImagesForRegion(ctx context.Context, payload CollectImagesForRegionP
 		return err
 	}
 
-	slog.Info("populated aws images", "region", region, "count", count)
-
-	return nil
-}
-
-// HandleCollectImagesTask collects the EC2 Images from all known regions.
-func HandleCollectImagesTask(ctx context.Context, t *asynq.Task) error {
-	var payload CollectImagesPayload
-	if err := yaml.Unmarshal(t.Payload(), &payload); err != nil {
-		return fmt.Errorf("yaml.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
-	}
-
-	if len(payload.ImageOwners) == 0 {
-		return ErrMissingOwners
-	}
-
-	return collectImages(ctx, payload)
-}
-
-func collectImages(ctx context.Context, payload CollectImagesPayload) error {
-	// Collect regions from Db
-	regions := make([]models.Region, 0)
-	err := db.DB.NewSelect().Model(&regions).Scan(ctx)
-	if err != nil {
-		slog.Error("could not select regions from db", "reason", err)
-		return err
-	}
-
-	for _, r := range regions {
-		// Trigger Asynq task for each region
-		collectImagesForRegionPayload := CollectImagesForRegionPayload{
-			Region:      r.Name,
-			ImageOwners: payload.ImageOwners,
-		}
-		imageTask, err := NewCollectImagesForRegionTask(collectImagesForRegionPayload)
-		if err != nil {
-			slog.Error("failed to create task", "reason", err)
-			continue
-		}
-
-		info, err := asynqclient.Client.Enqueue(imageTask)
-		if err != nil {
-			slog.Error(
-				"could not enqueue task",
-				"type", imageTask.Type(),
-				"region", r.Name,
-				"reason", err,
-			)
-			continue
-		}
-
-		slog.Info(
-			"enqueued task",
-			"type", imageTask.Type(),
-			"id", info.ID,
-			"queue", info.Queue,
-			"region", r.Name,
-		)
-	}
+	slog.Info(
+		"populated aws amis",
+		"region", payload.Region,
+		"account_id", payload.AccountID,
+		"count", count,
+	)
 
 	return nil
 }
