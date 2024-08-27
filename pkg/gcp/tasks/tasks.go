661af0543cb856661194ddaf417dace9974147fc
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-08-27 11:06:51 +0300

    gcp/tasks: initial commit of the `tasks` skeleton for GCP

---
+++ pkg/gcp/tasks/tasks.go
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tasks
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+
+	"github.com/gardener/inventory/pkg/clients/db"
+	"github.com/gardener/inventory/pkg/common/utils"
+	"github.com/gardener/inventory/pkg/core/registry"
+)
+
+const (
+	// TaskCollectAll is a meta task, which enqueues all relevant GCP tasks.
+	TaskCollectAll = "gcp:task:collect-all"
+
+	// TaskLinkAll is a task, which establishes links between GCP models.
+	TaskLinkAll = "gcp:task:link-all"
+)
+
+// HandleCollectAllTask is a handler, which enqueues tasks for collecting all
+// GCP objects.
+func HandleCollectAllTask(ctx context.Context, t *asynq.Task) error {
+	// Task constructors
+	taskFns := []utils.TaskConstructor{}
+
+	return utils.Enqueue(taskFns)
+}
+
+// HandleLinkAllTask is a handler, which establishes links between the various
+// GCP models.
+func HandleLinkAllTask(ctx context.Context, t *asynq.Task) error {
+	linkFns := []utils.LinkFunction{}
+
+	return utils.LinkObjects(ctx, db.DB, linkFns)
+}
+
+// init registers our task handlers and periodic tasks with the registries.
+func init() {
+	// Task handlers
+	registry.TaskRegistry.MustRegister(TaskCollectAll, asynq.HandlerFunc(HandleCollectAllTask))
+	registry.TaskRegistry.MustRegister(TaskLinkAll, asynq.HandlerFunc(HandleLinkAllTask))
+}
