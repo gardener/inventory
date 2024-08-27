4b85d8b6c7df821ffbcddfd8812f8c4effeb513b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-08-27 09:52:48 +0300

    gcp/models: initial commit of the GCP `Project` model

---
+++ pkg/gcp/models/models.go
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package models
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+
+	coremodels "github.com/gardener/inventory/pkg/core/models"
+	"github.com/gardener/inventory/pkg/core/registry"
+)
+
+// Project represents a GCP Project.
+type Project struct {
+	bun.BaseModel `bun:"table:gcp_project"`
+	coremodels.Model
+
+	Name              string    `bun:"name,notnull"`
+	Parent            string    `bun:"parent,notnull"`
+	ProjectID         string    `bun:"project_id,notnull,unique"`
+	State             string    `bun:"state,notnull"`
+	DisplayName       string    `bun:"display_name,notnull"`
+	ProjectCreateTime time.Time `bun:"project_create_time,nullzero"`
+	ProjectUpdateTime time.Time `bun:"project_update_time,nullzero"`
+	ProjectDeleteTime time.Time `bun:"project_delete_time,nullzero"`
+	Etag              string    `bun:"etag,notnull"`
+}
+
+func init() {
+	// Register the models with the default registry
+	registry.ModelRegistry.MustRegister("gcp:model:project", &Project{})
+
+	// Link tables
+}
