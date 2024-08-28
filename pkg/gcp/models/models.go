57bc20c94292b0851e5ccf5172297c734da96ed3
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-08-28 13:02:46 +0300

    gcp/models: add `bun` relationships between `Project` and `Instance` models

--- pkg/gcp/models/models.go
+++ pkg/gcp/models/models.go
@@ -24,13 +24,14 @@ type Project struct {
 	// ProjectID is the user-defined globally unique project id.
 	ProjectID string `bun:"project_id,notnull,unique"`
 
-	Parent            string    `bun:"parent,notnull"`
-	State             string    `bun:"state,notnull"`
-	DisplayName       string    `bun:"display_name,notnull"`
-	ProjectCreateTime time.Time `bun:"project_create_time,nullzero"`
-	ProjectUpdateTime time.Time `bun:"project_update_time,nullzero"`
-	ProjectDeleteTime time.Time `bun:"project_delete_time,nullzero"`
-	Etag              string    `bun:"etag,notnull"`
+	Parent            string      `bun:"parent,notnull"`
+	State             string      `bun:"state,notnull"`
+	DisplayName       string      `bun:"display_name,notnull"`
+	ProjectCreateTime time.Time   `bun:"project_create_time,nullzero"`
+	ProjectUpdateTime time.Time   `bun:"project_update_time,nullzero"`
+	ProjectDeleteTime time.Time   `bun:"project_delete_time,nullzero"`
+	Etag              string      `bun:"etag,notnull"`
+	Instances         []*Instance `bun:"rel:has-many,join:project_id=project_id"`
 }
 
 // Instance represents a GCP Instance.
@@ -38,25 +39,26 @@ type Instance struct {
 	bun.BaseModel `bun:"table:gcp_instance"`
 	coremodels.Model
 
-	Name                 string `bun:"name,notnull"`
-	Hostname             string `bun:"hostname,notnull"`
-	InstanceID           uint64 `bun:"instance_id,notnull,unique:gcp_instance_key"`
-	ProjectID            string `bun:"project_id,notnull,unique:gcp_instance_key"`
-	Zone                 string `bun:"zone,notnull"`
-	Region               string `bun:"region,notnull"`
-	CanIPForward         bool   `bun:"can_ip_forward,notnull"`
-	CPUPlatform          string `bun:"cpu_platform,notnull"`
-	CreationTimestamp    string `bun:"creation_timestamp,notnull"`
-	Description          string `bun:"description,notnull"`
-	LastStartTimestamp   string `bun:"last_start_timestamp,nullzero"`
-	LastStopTimestamp    string `bun:"last_stop_timestamp,nullzero"`
-	LastSuspendTimestamp string `bun:"last_suspend_timestamp,nullzero"`
-	MachineType          string `bun:"machine_type,notnull"`
-	MinCPUPlatform       string `bun:"min_cpu_platform,notnull"`
-	SelfLink             string `bun:"self_link,notnull"`
-	SourceMachineImage   string `bun:"source_machine_image,notnull"`
-	Status               string `bun:"status,notnull"`
-	StatusMessage        string `bun:"status_message,notnull"`
+	Name                 string   `bun:"name,notnull"`
+	Hostname             string   `bun:"hostname,notnull"`
+	InstanceID           uint64   `bun:"instance_id,notnull,unique:gcp_instance_key"`
+	ProjectID            string   `bun:"project_id,notnull,unique:gcp_instance_key"`
+	Zone                 string   `bun:"zone,notnull"`
+	Region               string   `bun:"region,notnull"`
+	CanIPForward         bool     `bun:"can_ip_forward,notnull"`
+	CPUPlatform          string   `bun:"cpu_platform,notnull"`
+	CreationTimestamp    string   `bun:"creation_timestamp,notnull"`
+	Description          string   `bun:"description,notnull"`
+	LastStartTimestamp   string   `bun:"last_start_timestamp,nullzero"`
+	LastStopTimestamp    string   `bun:"last_stop_timestamp,nullzero"`
+	LastSuspendTimestamp string   `bun:"last_suspend_timestamp,nullzero"`
+	MachineType          string   `bun:"machine_type,notnull"`
+	MinCPUPlatform       string   `bun:"min_cpu_platform,notnull"`
+	SelfLink             string   `bun:"self_link,notnull"`
+	SourceMachineImage   string   `bun:"source_machine_image,notnull"`
+	Status               string   `bun:"status,notnull"`
+	StatusMessage        string   `bun:"status_message,notnull"`
+	Project              *Project `bun:"rel:has-one,join:project_id=project_id"`
 }
 
 func init() {
