69cfc266ed488e04c5fe25b338ad06ef79e6f012
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-08-28 11:37:53 +0300

    gcp/constants: commit common GCP related constants

---
+++ pkg/gcp/constants/constants.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package constants
+
+const (
+	// PageSize represents the max number of items to fetch from the GCP API
+	// during a paginated call.
+	PageSize = 100
+
+	// ProjectsPrefix is the prefix of project identifiers.
+	ProjectsPrefix = "projects/"
+)
