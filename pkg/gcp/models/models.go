e8d64e1d9a48af54a155c5e458bd197bde4fdfff
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-09-02 11:33:50 +0300

    gcp/models: add `Address` model

--- pkg/gcp/models/models.go
+++ pkg/gcp/models/models.go
@@ -5,6 +5,7 @@
 package models
 
 import (
+	"net"
 	"time"
 
 	"github.com/uptrace/bun"
@@ -48,7 +49,7 @@ type Instance struct {
 	Region               string   `bun:"region,notnull"`
 	CanIPForward         bool     `bun:"can_ip_forward,notnull"`
 	CPUPlatform          string   `bun:"cpu_platform,notnull"`
-	CreationTimestamp    string   `bun:"creation_timestamp,notnull"`
+	CreationTimestamp    string   `bun:"creation_timestamp,nullzero"`
 	Description          string   `bun:"description,notnull"`
 	LastStartTimestamp   string   `bun:"last_start_timestamp,nullzero"`
 	LastStopTimestamp    string   `bun:"last_stop_timestamp,nullzero"`
@@ -98,11 +99,40 @@ type VPCToProject struct {
 	VPCID     uint64 `bun:"vpc_id,notnull,unique:l_gcp_vpc_to_project_key"`
 }
 
+// Address represents a GCP static IP address resource. The Address model
+// represents both - a global (external and internal) and regional (external and
+// internal) IP address.
+type Address struct {
+	bun.BaseModel `bun:"table:gcp_address"`
+	coremodels.Model
+
+	Address           net.IP   `bun:"address,notnull,type:varchar"`
+	AddressType       string   `bun:"address_type,notnull"`
+	IsGlobal          bool     `bun:"is_global,notnull"`
+	CreationTimestamp string   `bun:"creation_timestamp,nullzero"`
+	Description       string   `bun:"description,notnull"`
+	AddressID         uint64   `bun:"address_id,notnull,unique:gcp_address_key"`
+	ProjectID         string   `bun:"project_id,notnull,unique:gcp_address_key"`
+	Region            string   `bun:"region,notnull"`
+	IPVersion         string   `bun:"ip_version,notnull"`
+	IPv6EndpointType  string   `bun:"ipv6_endpoint_type,notnull"`
+	Name              string   `bun:"name,notnull"`
+	Network           string   `bun:"network,notnull"`
+	NetworkTier       string   `bun:"network_tier,notnull"`
+	Subnetwork        string   `bun:"subnetwork,notnull"`
+	PrefixLength      int      `bun:"prefix_length,notnull"`
+	Purpose           string   `bun:"purpose,notnull"`
+	SelfLink          string   `bun:"self_link,notnull"`
+	Status            string   `bun:"status,notnull"`
+	Project           *Project `bun:"rel:has-one,join:project_id=project_id"`
+}
+
 func init() {
 	// Register the models with the default registry
 	registry.ModelRegistry.MustRegister("gcp:model:project", &Project{})
 	registry.ModelRegistry.MustRegister("gcp:model:instance", &Instance{})
 	registry.ModelRegistry.MustRegister("gcp:model:vpc", &VPC{})
+	registry.ModelRegistry.MustRegister("gcp:model:address", &Address{})
 
 	// Link tables
 	registry.ModelRegistry.MustRegister("gcp:model:link_instance_to_project", &InstanceToProject{})
