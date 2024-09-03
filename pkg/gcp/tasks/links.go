52b0feaf16e5e2cf69f38dfbfee6cdacd96c8330
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-09-03 09:58:52 +0300

    gcp/tasks: link GCP `Address` with `Project` model

--- pkg/gcp/tasks/links.go
+++ pkg/gcp/tasks/links.go
@@ -110,3 +110,52 @@ func LinkVPCWithProject(ctx context.Context, db *bun.DB) error {
 
 	return nil
 }
+
+// LinkAddressWithProject creates links between the [models.Address] and
+// [models.Project] models.
+func LinkAddressWithProject(ctx context.Context, db *bun.DB) error {
+	var items []models.Address
+	err := db.NewSelect().
+		Model(&items).
+		Relation("Project").
+		Where("project.id IS NOT NULL").
+		Scan(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	links := make([]models.AddressToProject, 0, len(items))
+	for _, item := range items {
+		link := models.AddressToProject{
+			ProjectID: item.Project.ID,
+			AddressID: item.ID,
+		}
+		links = append(links, link)
+	}
+
+	if len(links) == 0 {
+		return nil
+	}
+
+	out, err := db.NewInsert().
+		Model(&links).
+		On("CONFLICT (project_id, address_id) DO UPDATE").
+		Set("updated_at = EXCLUDED.updated_at").
+		Returning("id").
+		Exec(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	count, err := out.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	logger := asynqutils.GetLogger(ctx)
+	logger.Info("linked gcp address with project", "count", count)
+
+	return nil
+}
