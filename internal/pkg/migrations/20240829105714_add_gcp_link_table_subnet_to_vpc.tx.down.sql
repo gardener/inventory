7aaa98fe016397d98125f5a3c05c5f600ba7a79f
Bobby-Wan <bozhidar.atanasov@sap.com>
2024-09-03 11:24:57 +0300

    feat(gcp/subnets): db migration files

---
+++ internal/pkg/migrations/20240829105714_add_gcp_link_table_subnet_to_vpc.tx.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS "l_gcp_subnet_to_vpc";
---
+++ internal/pkg/migrations/20240829105725_add_gcp_subnet.tx.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS "gcp_subnet" (
+    "subnet_id" bigint NOT NULL,
+    "vpc_name" varchar NOT NULL,
+    "project_id" varchar NOT NULL,
+    "name" varchar NOT NULL,
+    "region" varchar NOT NULL,
+    "creation_timestamp" varchar,
+    "description" varchar NOT NULL,
+    "ipv4_cidr_range" varchar NOT NULL,
+    "gateway" varchar NOT NULL,
+    "purpose" varchar NOT NULL,
+
+    "id" bigserial NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id"),
+    CONSTRAINT "gcp_subnet_key" UNIQUE ("subnet_id", "vpc_name", "project_id")
+);
