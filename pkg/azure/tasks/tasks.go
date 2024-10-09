eaa6de594e23502013284f99ec9133965903b807
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-10-09 09:27:52 +0300

    azure/tasks: initial commit of the `tasks` package for Azure

---
+++ pkg/azure/tasks/tasks.go
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tasks
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+
+	"github.com/gardener/inventory/pkg/clients/db"
+	"github.com/gardener/inventory/pkg/common/utils"
+	"github.com/gardener/inventory/pkg/core/registry"
+)
+
+const (
+	// TaskCollectAll is a meta task, which enqueues all relevant Azure tasks.
+	TaskCollectAll = "az:task:collect-all"
+
+	// TaskLinkAll is a task, which establishes links between Azure models.
+	TaskLinkAll = "az:task:link-all"
+)
+
+// HandleCollectAllTask is a handler, which enqueues tasks for collecting all
+// Azure objects.
+func HandleCollectAllTask(ctx context.Context, t *asynq.Task) error {
+	// Task constructors
+	taskFns := []utils.TaskConstructor{}
+
+	return utils.Enqueue(ctx, taskFns)
+}
+
+// HandleLinkAllTask is a handler, which establishes links between the various
+// Azure models.
+func HandleLinkAllTask(ctx context.Context, t *asynq.Task) error {
+	linkFns := []utils.LinkFunction{}
+
+	return utils.LinkObjects(ctx, db.DB, linkFns)
+}
+
+// init registers our task handlers and periodic tasks with the registries.
+func init() {
+	// Task handlers
+	registry.TaskRegistry.MustRegister(TaskCollectAll, asynq.HandlerFunc(HandleCollectAllTask))
+	registry.TaskRegistry.MustRegister(TaskLinkAll, asynq.HandlerFunc(HandleLinkAllTask))
+}
