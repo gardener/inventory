f8899fdeea3089a927c344c65dcdac9d3de52085
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-10-10 10:42:25 +0300

    azure/utils: initial commit of the Azure utils package

---
+++ pkg/azure/constants/constants.go
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package constants
+
+const (
+	// PowerStateUnknown represents a power state, which cannot be
+	// determined by inspecting the Virtual Machine instance view data.
+	PowerStateUnknown = "unknown"
+)
