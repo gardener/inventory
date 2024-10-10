058f7cae03f7e38e42c1e6f08b395ea290c310df
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-10-10 10:32:18 +0300

    azure/utils: commit `GetPowerState` util function

--- pkg/azure/utils/utils.go
+++ pkg/azure/utils/utils.go
@@ -6,9 +6,14 @@ package utils
 
 import (
 	"context"
+	"strings"
 
+	armcompute "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v6"
+
+	"github.com/gardener/inventory/pkg/azure/constants"
 	"github.com/gardener/inventory/pkg/azure/models"
 	"github.com/gardener/inventory/pkg/clients/db"
+	"github.com/gardener/inventory/pkg/utils/ptr"
 )
 
 // GetResourceGroupsFromDB returns the [models.ResourceGroup] from the database.
@@ -16,4 +21,22 @@ func GetResourceGroupsFromDB(ctx context.Context) ([]models.ResourceGroup, error
 	items := make([]models.ResourceGroup, 0)
 	err := db.DB.NewSelect().Model(&items).Scan(ctx)
 	return items, err
+}
+
+// GetPowerState returns the power state of a Virtual Machine by looking up the
+// provided states.
+func GetPowerState(states []*armcompute.InstanceViewStatus) string {
+	if states == nil {
+		return constants.PowerStateUnknown
+	}
+
+	powerStatePrefix := "PowerState/"
+	for _, state := range states {
+		code := ptr.Value(state.Code, "")
+		if strings.HasPrefix(code, powerStatePrefix) {
+			return strings.TrimPrefix(code, powerStatePrefix)
+		}
+	}
+
+	return constants.PowerStateUnknown
 }
