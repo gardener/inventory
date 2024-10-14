fddb0d7cb6c3b978b64b5e3da3467eae677083ca
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-10-14 10:07:16 +0300

    migrations: add schema for Azure Public IP Address

---
+++ internal/pkg/migrations/20241013071819_add_azure_public_ip_address_schema.tx.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS "az_public_address" (
+    "name" varchar NOT NULL,
+    "subscription_id" varchar NOT NULL,
+    "resource_group" varchar NOT NULL,
+    "location" varchar NOT NULL,
+    "provisioning_state" varchar NOT NULL,
+    "sku_name" varchar NOT NULL,
+    "sku_tier" varchar NOT NULL,
+    "ddos_protection" varchar,
+    "fqdn" varchar,
+    "reverse_fqdn" varchar,
+    "nat_gateway" varchar,
+    "ip_address" inet,
+    "ip_version" varchar,
+    "id" bigserial NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("id"),
+    CONSTRAINT "az_public_address_key" UNIQUE ("name", "subscription_id", "resource_group")
+);
