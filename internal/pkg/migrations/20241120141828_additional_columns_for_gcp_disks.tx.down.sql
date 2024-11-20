3fde8d74839372413b235c57d561ef5e7bcae12f
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-11-20 16:20:47 +0200

    migrations: additional columns for gcp_disk

---
+++ internal/pkg/migrations/20241120141828_additional_columns_for_gcp_disks.tx.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE gcp_disk DROP COLUMN last_attach_timestamp;
+ALTER TABLE gcp_disk DROP COLUMN last_detach_timestamp;
+ALTER TABLE gcp_disk DROP COLUMN status;
+ALTER TABLE gcp_disk DROP COLUMN size_gb;
