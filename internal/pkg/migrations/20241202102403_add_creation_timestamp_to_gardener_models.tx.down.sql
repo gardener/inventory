a2fbb170e0faa723d8ca2d170d101510767da094
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-12-02 12:37:33 +0200

    migrations: add `creation_timestamp` to all Gardener models

---
+++ internal/pkg/migrations/20241202102403_add_creation_timestamp_to_gardener_models.tx.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "g_project" DROP COLUMN creation_timestamp;
+ALTER TABLE "g_seed" DROP COLUMN creation_timestamp;
+ALTER TABLE "g_machine" DROP COLUMN creation_timestamp;
+ALTER TABLE "g_backup_bucket" DROP COLUMN creation_timestamp;
+ALTER TABLE "g_cloud_profile" DROP COLUMN creation_timestamp;
+ALTER TABLE "g_persistent_volume" DROP COLUMN creation_timestamp;
