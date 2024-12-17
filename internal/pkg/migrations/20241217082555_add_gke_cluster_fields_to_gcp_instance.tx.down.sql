6d6e0d86342161bb1449857726041b54d33a4fbe
Marin Atanasov Nikolov <dnaeon@gmail.com>
2024-12-17 10:27:16 +0200

    migrations: add gcp_instance.{gke_cluster_name,gke_pool_name} columns

---
+++ internal/pkg/migrations/20241217082555_add_gke_cluster_fields_to_gcp_instance.tx.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE gcp_instance DROP COLUMN gke_cluster_name;
+ALTER TABLE gcp_instance DROP COLUMN gke_pool_name;
---
+++ internal/pkg/migrations/20241217082555_add_gke_cluster_fields_to_gcp_instance.tx.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE gcp_instance ADD COLUMN gke_cluster_name VARCHAR;
+ALTER TABLE gcp_instance ADD COLUMN gke_pool_name VARCHAR;
