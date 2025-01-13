b3f07a1464ac6c66adf911ba0a0df90046ff4fc5
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-01-13 09:29:41 +0200

    feat(asynq/clients): add function for setting inspector at the level of package asynq

--- pkg/clients/asynq/asynq.go
+++ pkg/clients/asynq/asynq.go
@@ -7,9 +7,15 @@ package asynq
 import "github.com/hibiken/asynq"
 
 var Client *asynq.Client
+var Inspector *asynq.Inspector
 
 // SetClient shall be invoked from cli commands to set the asynq client for the workers.
 // Workers will have the ability to enqueue tasks.
 func SetClient(c *asynq.Client) {
 	Client = c
+}
+
+// SetInspector shall be invoked from cli commands to set the asynq inspector for the workers.
+func SetInspector(i *asynq.Inspector) {
+	Inspector = i
 }
