34b34e635ad71000339cdd53b7a50c7b4dc00f29
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-01-22 18:19:50 +0200

    g/tasks: add task for linking projects with members

--- pkg/gardener/tasks/links.go
+++ pkg/gardener/tasks/links.go
@@ -301,3 +301,52 @@ func LinkAzureImageWithCloudProfile(ctx context.Context, db *bun.DB) error {
 
 	return nil
 }
+
+// LinkProjectWithMember creates the relationship between the [models.Project]
+// and [models.ProjectMember] models.
+func LinkProjectWithMember(ctx context.Context, db *bun.DB) error {
+	var items []models.ProjectMember
+	err := db.NewSelect().
+		Model(&items).
+		Relation("Project").
+		Where("project.id IS NOT NULL").
+		Scan(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	links := make([]models.ProjectToMember, 0, len(items))
+	for _, item := range items {
+		link := models.ProjectToMember{
+			MemberID:  item.ID,
+			ProjectID: item.Project.ID,
+		}
+		links = append(links, link)
+	}
+
+	if len(links) == 0 {
+		return nil
+	}
+
+	out, err := db.NewInsert().
+		Model(&links).
+		On("CONFLICT (project_id, member_id) DO UPDATE").
+		Set("updated_at = EXCLUDED.updated_at").
+		Returning("id").
+		Exec(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	count, err := out.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	logger := asynqutils.GetLogger(ctx)
+	logger.Info("linked gardener project with member", "count", count)
+
+	return nil
+}
