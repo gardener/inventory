3b2682104eafce8d2650bce878953b5130dc263d
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-02-05 12:07:58 +0200

    feat(openstack): add openstack network, block_storage client registry

---
+++ pkg/clients/openstack/block_storage.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package openstack
+
+import (
+	"github.com/gardener/inventory/pkg/core/registry"
+    "github.com/gophercloud/gophercloud/v2"
+)
+
+// BlockStorageClientset provides the registry of OpenStack Block Storage API clients
+// for interfacing with block storage resources.
+var BlockStorageClientset = registry.New[string, Client[*gophercloud.ServiceClient]]()
