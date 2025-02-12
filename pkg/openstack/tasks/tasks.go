3864017b7bbd79456b91ff4a23c85b552f6d5d0c
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-02-12 09:04:18 +0200

    feat(openstack/tasks): add OpenStack tasks package + boilerplate for task registration.

---
+++ pkg/openstack/tasks/tasks.go
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tasks
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+
+	"github.com/gardener/inventory/pkg/clients/db"
+	"github.com/gardener/inventory/pkg/common/utils"
+	"github.com/gardener/inventory/pkg/core/registry"
+)
+
+const (
+	// TaskCollectAll is a meta task, which enqueues all relevant OpenStack
+	// tasks.
+	TaskCollectAll = "openstack:task:collect-all"
+
+	// TaskLinkAll is a task, which creates links between the OpenStack
+	// models.
+	TaskLinkAll = "openstack:task:link-all"
+)
+
+// HandleCollectAllTask is a handler, which enqueues tasks for collecting all
+// OpenStack objects.
+func HandleCollectAllTask(ctx context.Context, t *asynq.Task) error {
+	// Task constructors
+	taskFns := []utils.TaskConstructor{
+	}
+
+	return utils.Enqueue(ctx, taskFns)
+}
+
+// HandleLinkAllTask is a handler, which establishes links between the various
+// OpenStack models.
+func HandleLinkAllTask(ctx context.Context, t *asynq.Task) error {
+	linkFns := []utils.LinkFunction{
+	}
+
+	return utils.LinkObjects(ctx, db.DB, linkFns)
+}
+
+// init registers our task handlers and periodic tasks with the registries.
+func init() {
+	// Task handlers
+	registry.TaskRegistry.MustRegister(TaskCollectAll, asynq.HandlerFunc(HandleCollectAllTask))
+	registry.TaskRegistry.MustRegister(TaskLinkAll, asynq.HandlerFunc(HandleLinkAllTask))
+}
