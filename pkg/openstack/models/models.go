158af5a1ceb9637af70ad3b415b3f6dd3d92bad6
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-02-13 13:08:59 +0200

    feat(openstack): OpenStack server model

--- pkg/openstack/models/models.go
+++ pkg/openstack/models/models.go
@@ -4,6 +4,34 @@
 
 package models
 
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+
+	coremodels "github.com/gardener/inventory/pkg/core/models"
+	"github.com/gardener/inventory/pkg/core/registry"
+)
+
+// Server represents an OpenStack Server.
+type Server struct {
+	bun.BaseModel `bun:"table:openstack_server"`
+	coremodels.Model
+
+	ServerID         string    `bun:"server_id"`
+	Name             string    `bun:"name,notnull,unique"`
+	ProjectID        string    `bun:"project_id,notnull,unique"`
+	Domain           string    `bun:"domain,notnull"`
+	Region           string    `bun:"region,notnull"`
+	UserID           string    `bun:"user_id"`
+	AvailabilityZone string    `bun:"availability_zone"`
+	Status           string    `bun:"status"`
+	ImageID          string    `bun:"image_id"`
+	TimeCreated      time.Time `bun:"server_created_at"`
+	TimeUpdated      time.Time `bun:"server_updated_at"`
+}
+
 func init() {
 	// Register the models with the default registry
+	registry.ModelRegistry.MustRegister("openstack:model:server", &Server{})
 }
