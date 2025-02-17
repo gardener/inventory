46c7eae9f4cba48a5371fe640d12b70c60a04652
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-02-17 19:06:15 +0200

    fix(openstack/networks): schema/model make fields not null. use single project client

--- internal/pkg/migrations/20250212074416_add_openstack_networks.tx.up.sql
+++ internal/pkg/migrations/20250212074416_add_openstack_networks.tx.up.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS "openstack_network" (
     "domain" varchar NOT NULL,
     "region" varchar NOT NULL,
     "status" varchar NOT NULL,
-    "description" varchar,
-    "network_created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "network_updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" varchar NOT NULL,
+    "network_created_at" timestamptz NOT NULL,
+    "network_updated_at" timestamptz NOT NULL,
 
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id"),
-    UNIQUE ("network_id")
+    CONSTRAINT "openstack_network_key" UNIQUE ("network_id", "project_id")
 );
--- pkg/openstack/tasks/networks.go
+++ pkg/openstack/tasks/networks.go
@@ -7,6 +7,7 @@ package tasks
 import (
 	"context"
 	"encoding/json"
+	"errors"
 
 	asynqclient "github.com/gardener/inventory/pkg/clients/asynq"
 	"github.com/gardener/inventory/pkg/clients/db"
@@ -52,7 +53,12 @@ func HandleCollectNetworksTask(ctx context.Context, t *asynq.Task) error {
 		return asynqutils.SkipRetry(err)
 	}
 
+	if payload.ProjectID == "" {
+		return asynqutils.SkipRetry(errors.New("no project ID specified"))
+	}
+
 	return collectNetworks(ctx, payload)
+
 }
 
 // enqueueCollectNetworks enqueues tasks for collecting OpenStack Networks from
@@ -105,7 +111,7 @@ func enqueueCollectNetworks(ctx context.Context) error {
 
 	if err != nil {
 		logger.Error(
-			"could not get network clients",
+			"could not enqueue collection of networks",
 			"reason", err,
 		)
 	}
@@ -118,129 +124,110 @@ func enqueueCollectNetworks(ctx context.Context) error {
 func collectNetworks(ctx context.Context, payload CollectNetworksPayload) error {
 	logger := asynqutils.GetLogger(ctx)
 
-	projectClients := make([]*openstackclients.Client[*gophercloud.ServiceClient], 0)
-	err := openstackclients.NetworkClientset.Range(func(projectID string, client openstackclients.Client[*gophercloud.ServiceClient]) error {
-		if projectID == payload.ProjectID {
-			projectClients = append(projectClients, &client)
-		}
-		return nil
-	})
+	client, ok := openstackclients.NetworkClientset.Get(payload.ProjectID)
+	if !ok {
+		logger.Error(
+			"no client for given project",
+			"project_id", payload.ProjectID,
+		)
+		return asynqutils.SkipRetry(ClientNotFound(payload.ProjectID))
+	}
+
+	region := client.Region
+	domain := client.Domain
+	projectID := payload.ProjectID
+
+	logger.Info(
+		"collecting OpenStack networks",
+		"project_id", client.ProjectID,
+		"domain", client.Domain,
+		"region", client.Region,
+		"named_credentials", client.NamedCredentials,
+	)
+
+	items := make([]models.Network, 0)
+
+	err := networks.List(client.Client, nil).
+		EachPage(ctx,
+			func(ctx context.Context, page pagination.Page) (bool, error) {
+				networkList, err := networks.ExtractNetworks(page)
+
+				if err != nil {
+					logger.Error(
+						"could not extract networks pages",
+						"reason", err,
+					)
+					return false, err
+				}
+
+				for _, n := range networkList {
+					item := models.Network{
+						NetworkID:   n.ID,
+						Name:        n.Name,
+						ProjectID:   n.TenantID,
+						Domain:      domain,
+						Region:      region,
+						Status:      n.Status,
+						Description: n.Description,
+						TimeCreated: n.CreatedAt,
+						TimeUpdated: n.UpdatedAt,
+					}
+
+					items = append(items, item)
+				}
+
+				return true, nil
+			})
 
 	if err != nil {
 		logger.Error(
-			"could not get network clients",
-			"project_id", payload.ProjectID,
+			"could not extract network pages",
 			"reason", err,
 		)
 		return err
 	}
 
-	logger.Info(
-		"project clients found",
-		"project", payload.ProjectID,
-		"clients", len(projectClients),
-	)
-
-	if len(projectClients) == 0 {
-		return asynqutils.SkipRetry(ClientNotFound(payload.ProjectID))
+	if len(items) == 0 {
+		return nil
 	}
 
-	for _, client := range projectClients {
-		region := client.Region
-		domain := client.Domain
-		projectID := payload.ProjectID
+	out, err := db.DB.NewInsert().
+		Model(&items).
+		On("CONFLICT (network_id, project_id) DO UPDATE").
+		Set("name = EXCLUDED.name").
+		Set("domain = EXCLUDED.domain").
+		Set("region = EXCLUDED.region").
+		Set("status = EXCLUDED.status").
+		Set("description = EXCLUDED.description").
+		Set("network_created_at = EXCLUDED.network_created_at").
+		Set("network_updated_at = EXCLUDED.network_updated_at").
+		Set("updated_at = EXCLUDED.updated_at").
+		Returning("id").
+		Exec(ctx)
 
-		logger := asynqutils.GetLogger(ctx)
-
-		logger.Info(
-			"collecting OpenStack networks",
-			"project_id", client.ProjectID,
-			"domain", client.Domain,
-			"region", client.Region,
-			"named_credentials", client.NamedCredentials,
-		)
-
-		items := make([]models.Network, 0)
-
-		err := networks.List(client.Client, nil).
-			EachPage(ctx,
-				func(ctx context.Context, page pagination.Page) (bool, error) {
-					networkList, err := networks.ExtractNetworks(page)
-
-					if err != nil {
-						logger.Error(
-							"could not extract networks pages",
-							"reason", err,
-						)
-						return false, err
-					}
-
-					for _, n := range networkList {
-						item := models.Network{
-							NetworkID:   n.ID,
-							Name:        n.Name,
-							ProjectID:   n.TenantID,
-							Domain:      domain,
-							Region:      region,
-							Status:      n.Status,
-							Description: n.Description,
-							TimeCreated: n.CreatedAt,
-							TimeUpdated: n.UpdatedAt,
-						}
-
-						items = append(items, item)
-					}
-
-					return true, nil
-				})
-
-		if err != nil {
-			logger.Error(
-				"could not extract network pages",
-				"reason", err,
-			)
-			return err
-		}
-
-		out, err := db.DB.NewInsert().
-			Model(&items).
-			On("CONFLICT (network_id) DO UPDATE").
-			Set("name = EXCLUDED.name").
-			Set("project_id = EXCLUDED.project_id").
-			Set("domain = EXCLUDED.domain").
-			Set("region = EXCLUDED.region").
-			Set("status = EXCLUDED.status").
-			Set("description = EXCLUDED.description").
-			Set("network_created_at = EXCLUDED.network_created_at").
-			Set("network_updated_at = EXCLUDED.network_updated_at").
-			Set("updated_at = EXCLUDED.updated_at").
-			Returning("id").
-			Exec(ctx)
-
-		if err != nil {
-			logger.Error(
-				"could not insert networks into db",
-				"project_id", projectID,
-				"region", region,
-				"domain", domain,
-				"reason", err,
-			)
-			return err
-		}
-
-		count, err := out.RowsAffected()
-		if err != nil {
-			return err
-		}
-
-		logger.Info(
-			"populated openstack networks",
+	if err != nil {
+		logger.Error(
+			"could not insert networks into db",
 			"project_id", projectID,
 			"region", region,
 			"domain", domain,
-			"count", count,
+			"reason", err,
 		)
+		return err
 	}
+
+	count, err := out.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	logger.Info(
+		"populated openstack networks",
+		"project_id", projectID,
+		"region", region,
+		"domain", domain,
+		"count", count,
+	)
 
 	return nil
 }
