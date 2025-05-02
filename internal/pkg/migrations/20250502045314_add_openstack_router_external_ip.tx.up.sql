2c3fb96b89f76b9438ef530067cccf9e951ade10
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-05-02 08:45:40 +0300

    feat(openstack/router): add migration files for openstack_router_external_ip

---
+++ internal/pkg/migrations/20250502045314_add_openstack_router_external_ip.tx.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "openstack_router_external_ip" (
+    "router_id" varchar NOT NULL,
+    "external_ip" inet NOT NULL,
+    "external_subnet_id" varchar NOT NULL,
+    "project_id" varchar NOT NULL,
+
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id"),
+    CONSTRAINT "openstack_router_external_ip_key" UNIQUE ("external_ip", "external_subnet_id", "router_id")
+);
