d0e38549943f4f23fd5c372761b17309ab73c397
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-05-28 12:54:59 +0300

    remove local ids from openstack_server_with_subnet view

---
+++ internal/pkg/migrations/20250528095211_update_openstack_server_with_subnet.tx.up.sql
@@ -0,0 +1,25 @@
+DROP VIEW IF EXISTS "openstack_server_with_subnet";
+
+CREATE VIEW "openstack_server_with_subnet" AS
+SELECT 
+    s.server_id,
+    s.name as server_name,
+    s.project_id,
+    s.domain,
+    s.region,
+    s.availability_zone,
+    s.status,
+    s.image_id,
+    s.server_created_at,
+    s.server_updated_at,
+    subnet.subnet_id,
+    subnet.name as subnet_name,
+    subnet.network_id,
+    subnet.gateway_ip,
+    subnet.subnet_pool_id,
+    subnet.enable_dhcp,
+    subnet.ip_version
+FROM openstack_server as s
+INNER JOIN openstack_port AS p ON s.server_id = p.device_id
+INNER JOIN openstack_port_ip AS pip ON p.port_id = pip.port_id
+INNER JOIN openstack_subnet AS subnet ON pip.subnet_id = subnet.subnet_id;
