06f68c9d01936425b36f278e404dc9252a6c8286
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-06-05 15:45:20 +0300

    openstack_orphan_pool_member remove comments

--- internal/pkg/migrations/20250604073850_add_openstack_orphan_pool_member.tx.up.sql
+++ internal/pkg/migrations/20250604073850_add_openstack_orphan_pool_member.tx.up.sql
@@ -14,7 +14,3 @@ INNER JOIN openstack_pool AS p ON pm.project_id = p.project_id AND pm.pool_id = 
 LEFT JOIN openstack_server AS s ON pm.project_id = s.project_id AND pm.name = s.name
 LEFT JOIN g_shoot AS gs ON pm.inferred_gardener_shoot = gs.technical_id
 WHERE s.name IS NULL AND (gs.is_hibernated IS NULL OR gs.is_hibernated = false);
-
--- INNER JOIN openstack_pool AS p ON pm.project_id = p.project_id AND pm.pool_id = p.pool_id
--- LEFT JOIN openstack_server AS s ON pm.project_id = s.project_id AND pm.name = s.name
--- WHERE s.name IS NULL;
