e8479af88e920846b2c28ea18c09bb650dad4d5d
Marin Atanasov Nikolov <dnaeon@gmail.com>
2025-06-16 15:12:16 +0300

    gcp/tasks: instrument collection of forwarding rules

--- pkg/gcp/tasks/forwarding_rules.go
+++ pkg/gcp/tasks/forwarding_rules.go
@@ -13,6 +13,7 @@ import (
 	compute "cloud.google.com/go/compute/apiv1"
 	"cloud.google.com/go/compute/apiv1/computepb"
 	"github.com/hibiken/asynq"
+	"github.com/prometheus/client_golang/prometheus"
 	"google.golang.org/api/iterator"
 
 	asynqclient "github.com/gardener/inventory/pkg/clients/asynq"
@@ -22,6 +23,7 @@ import (
 	"github.com/gardener/inventory/pkg/gcp/constants"
 	"github.com/gardener/inventory/pkg/gcp/models"
 	gcputils "github.com/gardener/inventory/pkg/gcp/utils"
+	"github.com/gardener/inventory/pkg/metrics"
 	asynqutils "github.com/gardener/inventory/pkg/utils/asynq"
 )
 
@@ -131,6 +133,18 @@ func collectForwardingRules(ctx context.Context, payload CollectForwardingRulesP
 		return asynqutils.SkipRetry(ClientNotFound(payload.ProjectID))
 	}
 
+	var count int64
+	defer func() {
+		metric := prometheus.MustNewConstMetric(
+			forwardingRulesDesc,
+			prometheus.GaugeValue,
+			float64(count),
+			payload.ProjectID,
+		)
+		key := metrics.Key(TaskCollectForwardingRules, payload.ProjectID)
+		metrics.DefaultCollector.AddMetric(key, metric)
+	}()
+
 	logger := asynqutils.GetLogger(ctx)
 	logger.Info("collecting GCP forwarding rules", "project", payload.ProjectID)
 
@@ -231,7 +245,7 @@ func collectForwardingRules(ctx context.Context, payload CollectForwardingRulesP
 		return err
 	}
 
-	count, err := out.RowsAffected()
+	count, err = out.RowsAffected()
 	if err != nil {
 		return err
 	}
