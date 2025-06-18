452e44c7b0a05e4380e9abfb508c23973d3e4821
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-06-18 13:23:55 +0300

    fix(openstack): openstack_orphan_loadbalancer now filtered by orphaned pools

---
+++ internal/pkg/migrations/20250617123040_fix_openstack_orphan_loadbalancer.tx.down.sql
@@ -0,0 +1,18 @@
+DROP VIEW IF EXISTS "openstack_orphan_loadbalancer";
+
+CREATE OR REPLACE VIEW "openstack_orphan_loadbalancer" AS
+SELECT
+    lb.id,
+    lb.name,
+    lb.status,
+    lb.provider,
+    lb.vip_address,
+    lb.vip_network_id,
+    lb.vip_subnet_id,
+    lb.loadbalancer_created_at,
+    lb.loadbalancer_updated_at,
+    lb.project_id
+FROM openstack_loadbalancer as lb
+LEFT JOIN openstack_orphan_network as n
+ON lb.vip_network_id = n.network_id
+WHERE n.network_id IS NULL;
