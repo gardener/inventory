41ea1b4f587be6b1887514a3371c7d5960f1346c
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-06-19 12:43:08 +0300

    feat(openstack/metrics): expose metric for servers

--- pkg/openstack/tasks/metrics.go
+++ pkg/openstack/tasks/metrics.go
@@ -5,13 +5,24 @@
 package tasks
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+
 	"github.com/gardener/inventory/pkg/metrics"
 )
 
 var (
+	// serversDesc is the descriptor for a metric,
+	// which tracks the number of collected OpenStack Servers
+	serversDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(metrics.Namespace, "", "openstack_servers"),
+		"A gauge which tracks the number of collected OpenStack Servers",
+		[]string{"project", "domain", "region"},
+		nil,
+	)
 )
 
 func init() {
 	metrics.DefaultCollector.AddDesc(
+		serversDesc,
 	)
 }
