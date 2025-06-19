ba8099806bc5d2100f1f1969b05e12c8f677e12d
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-06-19 11:18:15 +0300

    feat(openstack): initial metric plumping (model names + metrics task with no descriptions

---
+++ pkg/openstack/tasks/metrics.go
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tasks
+
+import (
+	"github.com/gardener/inventory/pkg/metrics"
+)
+
+var (
+)
+
+func init() {
+	metrics.DefaultCollector.AddDesc(
+	)
+}
