7d32d3a03aedc3fbb386eef191f4c25154682165
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-08-14 15:32:53 +0300

    fix(openstack/server): metrics are fixed. remove debug logs

--- pkg/openstack/tasks/servers.go
+++ pkg/openstack/tasks/servers.go
@@ -142,14 +142,6 @@ func collectServers(ctx context.Context, payload CollectServersPayload) error {
 
 	var count int64
 	defer func() {
-		logger.Info(
-			"adding openstack server metrics",
-			"project", payload.Scope.Project,
-			"domain", payload.Scope.Domain,
-			"region", payload.Scope.Region,
-			"count", count,
-		)
-
 		metric := prometheus.MustNewConstMetric(
 			serversDesc,
 			prometheus.GaugeValue,
@@ -165,14 +157,6 @@ func collectServers(ctx context.Context, payload CollectServersPayload) error {
 			payload.Scope.Region,
 		)
 		metrics.DefaultCollector.AddMetric(key, metric)
-
-		logger.Info(
-			"successfully added openstack server metrics",
-			"project", payload.Scope.Project,
-			"domain", payload.Scope.Domain,
-			"region", payload.Scope.Region,
-			"count", count,
-		)
 	}()
 
 	items := make([]models.Server, 0)
