056790a3bf2612d607be8c301512c2913a0a6b83
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-08-27 09:15:56 +0300

    feat(aws/records): add migration files for dns records

---
+++ internal/pkg/migrations/20250825123850_add_aws_dns_record_table.tx.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS "aws_dns_record" (
+    "account_id" TEXT NOT NULL,
+    "hosted_zone_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "set_identifier" TEXT,
+    "is_alias" BOOLEAN NOT NULL,
+    "ttl" BIGINT,
+    "alias_dns_name" TEXT,
+    "evaluate_health" BOOLEAN,
+    "value" TEXT,
+
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("id"),
+    CONSTRAINT "aws_record_key" UNIQUE ("account_id", "hosted_zone_id", "name", "type", "set_identifier", "value")
+);
