99475ee28cd36a54cfc2e10d8744f79f4d2ff053
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-11-07 14:14:23 +0200

    feat(aws/dns-record): filter by provider_type field in g_dns_object and include 'aws-route-53' and 'remote' types

---
+++ internal/pkg/migrations/20251106133803_aws_orphan_dns_record_filter_by_provider_type.tx.up.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW "aws_orphan_dns_record" AS 
+SELECT
+    adr.name,
+    adr.type,
+    adr.value,
+    adr.account_id,
+    adr.hosted_zone_id
+FROM aws_dns_record as adr
+LEFT JOIN g_dns_object as gdo
+ON adr.name = gdo.fqdn || '.'
+AND (gdo.provider_type = 'aws-route53' OR gdo.provider_type = 'remote')
+AND adr.type NOT IN ('NS', 'SOA', 'MX', 'PTR')
+WHERE gdo.name IS NULL;
