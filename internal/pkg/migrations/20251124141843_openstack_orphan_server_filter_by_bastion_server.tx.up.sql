fd643bb4855d51f770d9f5aad4b5e78912a01f30
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-11-25 11:06:08 +0200

    feat(openstack/orphan-server-view): filter by bastion servers

---
+++ internal/pkg/migrations/20251124141843_openstack_orphan_server_filter_by_bastion_server.tx.up.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW "openstack_orphan_server" AS
+SELECT
+        s.server_id,
+        s.name,
+        s.project_id,
+        s.domain,
+        s.region,
+        s.user_id,
+        s.availability_zone,
+        s.status,
+        s.image_id,
+        s.server_created_at,
+        s.server_updated_at,
+        s.id,
+        s.created_at,
+        s.updated_at,
+        p.name as project_name
+FROM openstack_server AS s
+LEFT JOIN g_machine AS m ON s.name = m.name
+LEFT JOIN openstack_bastion_server AS bs ON s.server_id = bs.server_id
+INNER JOIN openstack_project as p on s.project_id = p.project_id
+WHERE m.name IS NULL;
