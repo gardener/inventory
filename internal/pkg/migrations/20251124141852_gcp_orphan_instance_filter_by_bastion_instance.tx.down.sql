70b576a8d8ad4b9660e918512ae11d9243925ffd
Bobby-Wan <bozhidar.atanasov@sap.com>
2025-11-25 11:06:45 +0200

    feat(gcp/migration): gcp_orphan_instance filter by gcp_bastion_instance

---
+++ internal/pkg/migrations/20251124141852_gcp_orphan_instance_filter_by_bastion_instance.tx.down.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW "gcp_orphan_instance" AS
+SELECT
+    i.id,
+    i.name,
+    i.hostname,
+    i.instance_id,
+    i.project_id,
+    i.region,
+    i.zone,
+    i.cpu_platform,
+    i.status,
+    i.status_message,
+    i.creation_timestamp,
+    i.description,
+    i.last_start_timestamp,
+    i.last_stop_timestamp,
+    i.last_suspend_timestamp,
+    i.machine_type,
+    i.gke_cluster_name,
+    i.gke_pool_name
+FROM gcp_instance AS i
+LEFT JOIN g_machine AS m ON i.name = m.name
+WHERE i.status = 'RUNNING' AND m.name IS NULL;
